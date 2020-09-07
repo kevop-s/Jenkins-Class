b19cfe28b8b9d0ea974e8d615d80cf8ca6b75a41
Carlos Zacarías Reyes Martínez <38059102+Carlos-ZRM@users.noreply.github.com>
2020-09-07 12:35:43 -0500

    Update runit.sh

--- clases/clase2/API-REST/runit.sh
+++ clases/clase2/API-REST/runit.sh
@@ -4,4 +4,18 @@ docker build -t flask-api .
 
 docker run -itd -p 8080:5000 flask-api
 
+#GET todos los items 
+curl http://localhost:5000/todos
 
+#GET un item 
+curl http://localhost:5000/todos/todo3
+
+#DELETE un item 
+curl http://localhost:5000/todos/todo2 -X DELETE -v
+
+# POST 
+curl http://localhost:5000/todos -d "task=sNuevo texto" -X POST -v
+
+# PUT 
+
+curl http://localhost:5000/todos/todo3 -d "task=something different" -X PUT -v
